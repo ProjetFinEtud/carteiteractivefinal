3748e2e7357aa64308da2d9a882798adeadc297d
Abdoulaye BERTE <abdouberte97@gmail.com>
2024-03-14 01:26:53 +0100

    create texte dynamique acc

--- src/Components/Carrousel.js
+++ src/Components/Carrousel.js
@@ -1,35 +1,61 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Carousel from "react-material-ui-carousel";
 import { Paper, Typography } from "@mui/material";
-import "./carousel.css"; // Importez vos styles CSS personnalisés ici
+import "./carousel.css";
 
 function CarouselHome() {
-  const items = [
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    const fetchHomeData = async () => {
+      try {
+        const response = await fetch("/server/user/allAcc");
+        if (!response.ok) {
+          throw new Error("Erreur lors de la récupération des données");
+        }
+        const data = await response.json();
+        if (data.length === 0) {
+          // Si la table est vide, utilisez les données statiques
+          setItems(staticItems);
+        } else {
+          // Sinon, utilisez les données de la table
+          setItems(data);
+        }
+      } catch (error) {
+        console.error("Erreur:", error.message);
+      }
+    };
+
+    fetchHomeData();
+  }, []);
+
+  // Données statiques à afficher si la table est vide
+  const staticItems = [
     {
-      title: "Contexte de l'application",
-      description:
+      acc_tire: "Contexte de l'application",
+      acc_texte:
         "De tout temps, les établissements d'enseignement supérieur ont joué un rôle crucial dans la formation et l'épanouissement des individus...",
     },
     {
-      title: "Défis des diplômés",
-      description:
+      acc_titre: "Défis des diplômés",
+      acc_texte:
         "Lorsqu'il s'agit de transition vers le monde professionnel, les diplômés font face à divers défis...",
     },
     {
-      title: "Objectif de l'application",
-      description:
+      acc_titre: "Objectif de l'application",
+      acc_texte:
         "Cette application vise principalement à faciliter l'insertion professionnelle des anciens élèves en mettant en relation les diplômés...",
     },
     {
-      title: "Fonctionnalités de l'application",
-      description:
+      acc_titre: "Fonctionnalités de l'application",
+      acc_texte:
         "Les fonctionnalités comprennent la possibilité pour les anciens élèves de partager leur expérience, mettre en avant leur parcours...",
     },
   ];
 
   return (
     <div className="carousel-container">
-      <Carousel >
+      <Carousel>
         {items.map((item, index) => (
           <Item key={index} item={item} />
         ))}
@@ -38,19 +64,16 @@ function CarouselHome() {
   );
 }
 
-function Item(props) {
+function Item({ item }) {
   return (
-    <>
     <Paper>
       <Typography variant="h1" gutterBottom className="carousel-title">
-        {props.item.title}
+        {item.acc_titre}
       </Typography>
       <Typography variant="body1" paragraph className="carousel-description">
-        {props.item.description}
+        {item.acc_texte}
       </Typography>
     </Paper>
-    </>
-  
   );
 }
 
